3acca2b6f7b99efee8ea5dc3c06c38096602c480
matinslcs <ccplucasmartins@gmail.com>
2025-07-13 14:32:29 -0300

    first commit

---
+++ EmprestimoLivros/Models/EmprestimosModel.cs
@@ -0,0 +1,13 @@
+﻿namespace EmprestimoLivros.Models
+{
+    public class EmprestimosModel
+    {
+        public int Id { get; set; }
+        public string Recebedor { get; set; }
+        public String Fornecedor { get; set; }
+        public String LivroEmprestado { get; set; }
+        public DateTime DataEmprestimo { get; set; } = DateTime.Now;
+
+
+    }
+}
