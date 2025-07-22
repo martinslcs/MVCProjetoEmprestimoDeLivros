ec352ea8306122c8376974a08be7ed405802d6fc
matinslcs <ccplucasmartins@gmail.com>
2025-07-21 21:51:06 -0300

    Adicionando funcionalidades

--- EmprestimoLivros/Controllers/EmprestimoController.cs
+++ EmprestimoLivros/Controllers/EmprestimoController.cs
@@ -12,13 +12,35 @@ namespace EmprestimoLivros.Controllers
         public EmprestimoController(ApplicationDbContext db)
         {
             _db = db;
-            
+
         }
         public IActionResult Index()
         {
             IEnumerable<EmprestimosModel> emprestimos = _db.Emprestimos;
 
             return View(emprestimos);
+        }
+
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(EmprestimosModel emprestimos)
+        {
+            if (ModelState.IsValid) 
+            {
+                _db.Emprestimos.Add(emprestimos);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
         }
     }
 }
