b5d63db1f4c2681bf58676464c0265e6940ea350
matinslcs <ccplucasmartins@gmail.com>
2025-07-26 00:17:38 -0300

    Adiciona funcionalidade de busca e filtro no CRUD de empréstimos

--- EmprestimoLivros/Controllers/EmprestimoController.cs
+++ EmprestimoLivros/Controllers/EmprestimoController.cs
@@ -29,18 +29,100 @@ namespace EmprestimoLivros.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Editar(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+
+            }
+
+            EmprestimosModel emprestimo = _db.Emprestimos.FirstOrDefault(x => x.Id == id);
+
+            if (emprestimo == null)
+            {
+                return NotFound();
+            }
+
+            return View(emprestimo);
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+
+            }
+
+            EmprestimosModel emprestimo = _db.Emprestimos.FirstOrDefault(x => x.Id == id);
+
+            if (emprestimo == null)
+            {
+                return NotFound();
+            }
+
+            return View(emprestimo);
+        }
+
+
+
         [HttpPost]
         public IActionResult Cadastrar(EmprestimosModel emprestimos)
         {
-            if (ModelState.IsValid) 
+            if (ModelState.IsValid)
             {
                 _db.Emprestimos.Add(emprestimos);
                 _db.SaveChanges();
 
+                TempData["MenssagemSucesso"] = "Cadastro realizado com sucesso!";
+
                 return RedirectToAction("Index");
             }
 
+            TempData["MenssagemErro"] = "Ocorreu algum erro ao Cadastrar!";
+
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Editar(EmprestimosModel emprestimo)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Emprestimos.Update(emprestimo);
+                _db.SaveChanges();
+
+                TempData["MenssagemSucesso"] = "Edição realizada com sucesso!";
+
+                return RedirectToAction("Index");
+            }
+
+            TempData["MenssagemErro"] = "Ocorreu algum erro ao editar!";
+
+            return View(emprestimo);
+        }
+
+        [HttpPost]
+        public IActionResult Excluir(EmprestimosModel emprestimo)
+        {
+            if (emprestimo == null) 
+            {
+                return NotFound();
+            }
+
+            _db.Emprestimos.Remove(emprestimo);
+            _db.SaveChanges();
+
+            TempData["MenssagemSucesso"] = "Remoção realizada com sucesso!";
+
+            TempData["MenssagemErro"] = "Ocorreu algum erro ao excluir!";
+
+            return RedirectToAction("Index");
+        
+        }
+
     }
 }
--- EmprestimoLivros/wwwroot/js/site.js
+++ EmprestimoLivros/wwwroot/js/site.js
@@ -1,4 +1,34 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    $('#Emprestimos').DataTable({
+        language: {
+            "decimal": "",
+            "emptyTable": "Nenhum dado disponível na tabela",
+            "info": "Mostrando _START_ até _END_ de _TOTAL_ entradas",
+            "infoEmpty": "Mostrando 0 até 0 de 0 entradas",
+            "infoFiltered": "(filtrado de _MAX_ entradas totais)",
+            "infoPostFix": "",
+            "thousands": ",",
+            "lengthMenu": "Mostrar _MENU_ entradas",
+            "loadingRecords": "Carregando...",
+            "processing": "",
+            "search": "Procurar:",
+            "zeroRecords": "Nenhum registro correspondente encontrado",
+            "paginate": {
+                "first": "Primeiro",
+                "last": "Último",
+                "next": "Próximo",
+                "previous": "Anterior"
+            },
+            "aria": {
+                "orderable": "Ordenar por esta coluna",
+                "orderableReverse": "Inverter ordenação da coluna"
+            }
+        }
+    });
 
-// Write your JavaScript code.
+    setTimeout(function () {
+        $(".alert").fadeOut("slow", function () {
+            $(this).alert('close');
+        });
+    }, 5000);
+});
