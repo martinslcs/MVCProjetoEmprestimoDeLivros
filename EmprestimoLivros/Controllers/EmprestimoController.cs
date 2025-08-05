826210780aee0c501162a42447918929e80afaac
matinslcs <ccplucasmartins@gmail.com>
2025-08-04 23:24:33 -0300

    Adicionando funcionalidade de exportar relatorio

--- EmprestimoLivros/Controllers/EmprestimoController.cs
+++ EmprestimoLivros/Controllers/EmprestimoController.cs
@@ -1,6 +1,8 @@
-﻿using EmprestimoLivros.Data;
+﻿using ClosedXML.Excel;
+using EmprestimoLivros.Data;
 using EmprestimoLivros.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
 
 namespace EmprestimoLivros.Controllers
 {
@@ -67,6 +69,50 @@ namespace EmprestimoLivros.Controllers
             return View(emprestimo);
         }
 
+
+        [HttpGet]
+        public IActionResult Exportar()
+        {
+            var dados = GetDados();
+
+            using (XLWorkbook workbook = new XLWorkbook())
+            {
+                workbook.AddWorksheet(dados, "Dados Empréstimos");
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    workbook.SaveAs(ms);
+                    return File(ms.ToArray(), "application/vnd.openxmlformats-officedocument.spredsheetml.sheet", "Emprestimos.xls");
+                }
+            }
+        }
+
+        private DataTable GetDados()
+        {
+
+            DataTable dataTable = new DataTable();
+
+            dataTable.TableName = "Dados empréstimos";
+            dataTable.Columns.Add("Recebedor", typeof(string));
+            dataTable.Columns.Add("Fornecedor", typeof(string));
+            dataTable.Columns.Add("Livro", typeof(string));
+            dataTable.Columns.Add("Data empréstimo", typeof(DateTime));
+
+            var dados = _db.Emprestimos.ToList();
+
+            if (dados.Count > 0)
+            {
+                dados.ForEach(emprestimo =>
+                {
+                    dataTable.Rows.Add(emprestimo.Recebedor, emprestimo.Fornecedor, emprestimo.LivroEmprestado, emprestimo.DataUltimaAtualizacao);
+
+                });
+            }
+
+
+            return dataTable;
+
+        }
 
 
         [HttpPost]
